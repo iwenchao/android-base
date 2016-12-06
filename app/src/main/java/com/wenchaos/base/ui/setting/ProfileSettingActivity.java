ce0f2705c641e6053bacc734c06c52a40f37f4f9
iwen <iwenchaos@gmail.com>
2016-12-06 22:29:28 +0800

    新建个人设置模块

---
+++ app/src/main/java/com/wenchaos/base/ui/setting/ProfileSettingActivity.java
@@ -0,0 +1,25 @@
+package com.wenchaos.base.ui.setting;
+
+import com.wenchaos.base.base.BaseActivity;
+
+/**
+ * Created by iwen
+ * on 16-12-6.
+ */
+
+public class ProfileSettingActivity extends BaseActivity {
+    @Override
+    public int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initPresent() {
+
+    }
+}
---
+++ app/src/main/java/com/wenchaos/base/ui/setting/ProfileSettingFragment.java
@@ -0,0 +1,17 @@
+package com.wenchaos.base.ui.setting;
+
+import com.wenchaos.base.base.BaseFragment;
+
+/**
+ * Created by iwen
+ * on 16-12-6.
+ *
+ * @email: iwenchaos@gmail.com
+ * @Description:
+ */
+
+public class ProfileSettingFragment extends BaseFragment {
+
+
+    
+}
